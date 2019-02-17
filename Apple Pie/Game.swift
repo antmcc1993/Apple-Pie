c92b2f4773e81625ad2cbb879a4418ca2b7a4edc
Anthony McClendon <anthonymcclendon@anthonys-macbook-air.local>
2019-02-17 16:41:16 -0600

    Added newRound and updateUI methods

---
+++ Apple Pie/Game.swift
@@ -0,0 +1,15 @@
+//
+//  Game.swift
+//  Apple Pie
+//
+//  Created by Anthony McClendon on 2/16/19.
+//  Copyright © 2019 Anthony McClendon. All rights reserved.
+//
+
+import Foundation
+
+
+struct Game {
+    var word : String
+    var incorrectMovesRemaining : Int
+}
